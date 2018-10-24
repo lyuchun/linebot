bea8cd408d2d922f1d57a6ca64343a19203011f4
lyuchun <44165053+lyuchun@users.noreply.github.com>
2018-10-24 14:59:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='Nice to meet you!')
+    message = TextSendMessage(text=(event.source.user_id))
     message2 = TextSendMessage(text=(event.source.user_id)) #reply userid
     line_bot_api.reply_message(event.reply_token, message)  
     # message = TextSendMessage(text=event)
